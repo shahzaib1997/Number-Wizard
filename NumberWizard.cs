5e02b91d6e8e75926ccf13d3f98462eb4c08759d
Shahzaib Zafar Cheema <shahzaib.cheema16121997@gmail.com>
2019-08-24 00:06:07 +0500

    Add files via upload

---
+++ NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max, min;
+    int guess;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        if (guess != max)
+        {
+            min = guess + 1;
+        }
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        if (guess != min)
+        {
+            max = guess - 1;
+        }
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
